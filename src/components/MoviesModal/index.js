ae50b04fadc7d2327aac3a0ec7f8b3e787ab8625
itslitilinchpin2 <itslitulinchpin@gmail.com>
2023-10-31 00:52:01 +0100

    fix rendering error at MoviesModal

--- src/components/MoviesModal/index.js
+++ src/components/MoviesModal/index.js
@@ -4,14 +4,14 @@ import useOnClickOutside from '../../hooks/useOnClickOutside';
 import { useRef } from "react";
 
 export default function MovieModal(
-  backdrop_path,
+  {backdrop_path,
   title,
   overview,
   name,
   release_date,
   first_air_date,
   vote_average,
-  setModalOpen,
+  setModalOpen}
 ) {
 
   const ref = useRef();
